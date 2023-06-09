8631ebb9d2b6c635c2c7129eb69e3f60a9fe6a16
Sayunkta Mankar <sayumankar77@gmail.com>
2023-06-09 10:56:49 +0530

    Add

---
+++ LinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList");
+
+            LinkedList list = new LinkedList();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
